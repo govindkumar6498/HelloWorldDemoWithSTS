cc260946627a42978d1fda20bb70631ec214ccb0
govind6498 <govindsingh6498@gail.com>
2021-11-29 14:04:43 +0530

    ADDED:TO Initialize project structure

---
+++ src/main/java/com/govind/hellowolrddemo/HelloWolrdDemoApplication.java
@@ -0,0 +1,13 @@
+package com.govind.hellowolrddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWolrdDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWolrdDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/govind/hellowolrddemo/HelloWolrdDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.govind.hellowolrddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWolrdDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
