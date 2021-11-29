3d3d964fb9b513a9b9327da620c507a580c0fc89
govind6498 <govindsingh6498@gail.com>
2021-11-29 14:07:04 +0530

    ADDED:To Display Hello World

--- src/main/java/com/govind/hellowolrddemo/HelloWolrdDemoApplication.java
+++ src/main/java/com/govind/hellowolrddemo/HelloWolrdDemoApplication.java
@@ -8,6 +8,7 @@ public class HelloWolrdDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloWolrdDemoApplication.class, args);
+		System.out.println("Hello Govind Kumar");
 	}
 
 }
